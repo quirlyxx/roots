bc09d05f17fe3595447e7bde6b84e47aec297449
User <User@DESKTOP-HNA01EJ>
2025-06-19 12:19:35 +0300

    commit

--- roots/main.cpp
+++ roots/main.cpp
@@ -2,6 +2,34 @@
 #include <cmath>
 using namespace std;
 
+class EquationException {
+public:
+    virtual const char* what() const {
+        return "Equation error occurred.";
+    }
+};
+
+class DivisionByZeroException : public EquationException {
+public:
+    const char* what() const override {
+        return "Cannot divide by zero (a = 0).";
+    }
+};
+
+class NoRealRootsException : public EquationException {
+public:
+    const char* what() const override {
+        return "No real roots (discriminant < 0).";
+    }
+};
+
+class InfiniteSolutionsException : public EquationException {
+public:
+    const char* what() const override {
+        return "Infinite number of solutions (0x = 0).";
+    }
+};
+
 class Equation {
 public:
     virtual void findRoots() const = 0;
@@ -17,16 +45,14 @@ public:
     void findRoots() const override {
         if (a == 0) {
             if (b == 0) {
-                cout << "Linear. Infinite number of solutions.\n";
+                throw InfiniteSolutionsException();
             }
             else {
-                cout << "Linear. No solution.\n";
+                throw DivisionByZeroException();
             }
         }
-        else {
-            double x = -b / a;
-            cout << "Linear. Root: x = " << x << endl;
-        }
+        double x = -b / a;
+        cout << "Linear. Root: x = " << x << endl;
     }
 };
 
@@ -38,6 +64,7 @@ public:
 
     void findRoots() const override {
         if (a == 0) {
+            // Öå âæå ë³í³éíå ð³âíÿííÿ
             LinearEquation lin(b, c);
             lin.findRoots();
             return;
@@ -55,24 +82,27 @@ public:
             cout << "Quadratic Root. x = " << x << endl;
         }
         else {
-            cout << "Quadratic. No roots.\n";
+            throw NoRealRootsException();
         }
     }
 };
 
 int main() {
-    Equation* eq1 = new LinearEquation(4, -7);
-    Equation* eq2 = new QuadraticEquation(2, -4, 5);
-    Equation* eq3 = new QuadraticEquation(4,3, 1);
-    Equation* eq4 = new LinearEquation(0, 0);
+    Equation* equations[4];
+    equations[0] = new LinearEquation(4, -7);     
+    equations[1] = new QuadraticEquation(2, -4, 5);  
+    equations[2] = new QuadraticEquation(4, 3, 1);
+    equations[3] = new LinearEquation(0, 0); 
 
-    eq1->findRoots();
-    eq2->findRoots();
-    eq3->findRoots();
-    eq4->findRoots();
+    for (int i = 0; i < 4; ++i) {
+        try {
+            equations[i]->findRoots();
+        }
+        catch (EquationException& e) {
+            cout << "Exception: " << e.what() << endl;
+        }
+        delete equations[i];
+    }
 
-    delete eq1;
-    delete eq2;
-    delete eq3;
-    delete eq4;
+    return 0;
 }
